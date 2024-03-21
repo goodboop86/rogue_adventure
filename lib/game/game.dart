c8d87c920ed143887b0346c14c5e203af208bef1
goodboop86 <goodboop86@gmail.com>
2024-03-22 00:07:44 +0900

    minimum game

---
+++ lib/game/game.dart
@@ -0,0 +1,12 @@
+
+import 'package:flame/components.dart';
+import 'package:flame/events.dart';
+import 'package:flame/game.dart';
+
+class MainGame extends FlameGame with KeyboardEvents, HasGameRef {
+
+  @override
+  Future<void> onLoad() async {
+    super.onLoad();
+  }
+}
